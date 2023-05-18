fa7cff887bb83ceba251e8741d0e6bacd4e845bf
Manikanda Prabhu <manikandan@oceansoftwares.in>
2023-05-18 10:25:37 +0530

    changes

--- controller/Controllers/EmployerTypeController.js
+++ controller/Controllers/EmployerTypeController.js
@@ -4,13 +4,13 @@ const authorizer = require("../../middleware/authorizer");
 const Auth = require("../../Helpers/Auth");
 
 const getEmployerTypeData = async (req, res, next) => {
-  if (await Auth.authorizer(req, res, next)) {
-    var data = await PostEventModel.getEmployerTypeData(req, res);
-    // console.log(data)
-    res.status(data.statusCode).send(data);
-  } else {
-    res.status(400).send({ msg: "invalid sessions" });
-  }
+  // if (await Auth.authorizer(req, res, next)) {
+  var data = await PostEventModel.getEmployerTypeData(req, res);
+  // console.log(data)
+  res.status(data.statusCode).send(data);
+  // } else {
+  //   res.status(400).send({ msg: "invalid sessions" });
+  // }
 };
 
 const saveEmployerType = async (req, res, next) => {
